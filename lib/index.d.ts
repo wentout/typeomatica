86bb9e6c11975ff3670145f378b1d97ae525b5a8
wentout <went.out@gmail.com>
2023-03-21 12:56:04 +0300

    deps update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,7 @@
 declare const BaseConstructor: ObjectConstructor;
 export declare class BaseClass extends BaseConstructor {
 }
-export declare type IDEF<T, P = {}, R = {}> = {
+export type IDEF<T, P = {}, R = {}> = {
     new (...args: unknown[]): T;
     (this: T, ...args: unknown[]): R;
     prototype: P;
