fc2c56c3aa648c6bddf6e55533d244589846b2f3
wentout <went.out@gmail.com>
2024-04-07 21:05:42 +0300

    experimenting with node builtin test runner

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	testMatch: ['**/test/**/index.ts', '**/test/**/addition.js'],
+	testMatch: ['**/test/**/index.ts'],
+	// testMatch: ['**/test/**/index.ts', '**/test/**/addition.js'],
 	transform       : {
 		'\\./test/*.ts$': ['ts-jest', { tsconfig : './tsconfig.jest.json' }]
 	}
